61452b2d9b81b0fa742ebcc1b4812aae1d43e164
esthjung <esthjung@redvelvet.biostat.berkeley.edu>
2023-04-04 17:35:31 -0700

    Elicit histograms

--- 1-data-cleaning/1_combine_datasets.R
+++ 1-data-cleaning/1_combine_datasets.R
@@ -169,6 +169,9 @@ vital_raw <- left_join(vital_raw, vital_ids, by = "ID")
 
 #Merge in with harmonized data set
 vitalmerged <- left_join(vital, vital_raw, by = "SUBJIDO")
+unique(vitalmerged$dob)
+unique(vitalmerged$dov)
+colnames(vitalmerged)
 
 #-------------------------------------------------------------------
 # misame raw data
@@ -216,8 +219,6 @@ table(d$visit[d$studyid=="VITAL-Lactation"])
 #Check measurement frequency
 meas_freq_tab <- d %>% filter(!is.na(waz)|!is.na(haz)) %>% group_by(studyid, country, subjid) %>% mutate(lagage=agedays-lag(agedays)) %>% group_by(studyid) %>% summarize(mn=mean(lagage,na.rm=T), md=median(lagage,na.rm=T))
 meas_freq_tab
-
-
 
 
 
